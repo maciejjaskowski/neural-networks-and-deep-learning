c165f422da85116a233d32f6762248f95b881a95
Maciej Jaskowski <maciej.jaskowski@gmail.com>
2015-11-18 11:48:52 +0100

    Remove drop out to simplify the Network

--- src/network3.py
+++ src/network3.py
@@ -21,9 +21,9 @@ modifiable.  It is not optimized, and omits many desirable features.
 
 This program incorporates ideas from the Theano documentation on
 convolutional neural nets (notably,
-http://deeplearning.net/tutorial/lenet.html ), from Misha Denil's
-implementation of dropout (https://github.com/mdenil/dropout ), and
-from Chris Olah (http://colah.github.io ).
+http://deeplearning.net/tutorial/lenet.html ), 
+### from Misha Denil's implementation of dropout (https://github.com/mdenil/dropout ), 
+and from Chris Olah (http://colah.github.io ).
 
 """
 
@@ -49,7 +49,7 @@ from theano.tensor import tanh
 
 
 #### Constants
-GPU = True
+GPU = False
 if GPU:
     print "Trying to run under a GPU.  If this is not desired, then modify "+\
         "network3.py\nto set the GPU flag to False."
@@ -92,13 +92,12 @@ class Network(object):
         self.x = T.matrix("x")
         self.y = T.ivector("y")
         init_layer = self.layers[0]
-        init_layer.set_inpt(self.x, self.x, self.mini_batch_size)
+        init_layer.set_inpt(self.x, self.mini_batch_size)
         for j in xrange(1, len(self.layers)):
             prev_layer, layer  = self.layers[j-1], self.layers[j]
             layer.set_inpt(
-                prev_layer.output, prev_layer.output_dropout, self.mini_batch_size)
+                prev_layer.output, self.mini_batch_size)
         self.output = self.layers[-1].output
-        self.output_dropout = self.layers[-1].output_dropout
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             validation_data, test_data, lmbda=0.0):
@@ -222,7 +221,7 @@ class ConvPoolLayer(object):
             borrow=True)
         self.params = [self.w, self.b]
 
-    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+    def set_inpt(self, inpt, mini_batch_size):
         self.inpt = inpt.reshape(self.image_shape)
         conv_out = conv.conv2d(
             input=self.inpt, filters=self.w, filter_shape=self.filter_shape,
@@ -231,15 +230,13 @@ class ConvPoolLayer(object):
             input=conv_out, ds=self.poolsize, ignore_border=True)
         self.output = self.activation_fn(
             pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-        self.output_dropout = self.output # no dropout in the convolutional layers
 
 class FullyConnectedLayer(object):
 
-    def __init__(self, n_in, n_out, activation_fn=sigmoid, p_dropout=0.0):
+    def __init__(self, n_in, n_out, activation_fn=sigmoid):
         self.n_in = n_in
         self.n_out = n_out
         self.activation_fn = activation_fn
-        self.p_dropout = p_dropout
         # Initialize weights and biases
         self.w = theano.shared(
             np.asarray(
@@ -253,15 +250,10 @@ class FullyConnectedLayer(object):
             name='b', borrow=True)
         self.params = [self.w, self.b]
 
-    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+    def set_inpt(self, inpt, mini_batch_size):
         self.inpt = inpt.reshape((mini_batch_size, self.n_in))
-        self.output = self.activation_fn(
-            (1-self.p_dropout)*T.dot(self.inpt, self.w) + self.b)
+        self.output = self.activation_fn(T.dot(self.inpt, self.w) + self.b)
         self.y_out = T.argmax(self.output, axis=1)
-        self.inpt_dropout = dropout_layer(
-            inpt_dropout.reshape((mini_batch_size, self.n_in)), self.p_dropout)
-        self.output_dropout = self.activation_fn(
-            T.dot(self.inpt_dropout, self.w) + self.b)
 
     def accuracy(self, y):
         "Return the accuracy for the mini-batch."
@@ -269,10 +261,9 @@ class FullyConnectedLayer(object):
 
 class SoftmaxLayer(object):
 
-    def __init__(self, n_in, n_out, p_dropout=0.0):
+    def __init__(self, n_in, n_out):
         self.n_in = n_in
         self.n_out = n_out
-        self.p_dropout = p_dropout
         # Initialize weights and biases
         self.w = theano.shared(
             np.zeros((n_in, n_out), dtype=theano.config.floatX),
@@ -282,17 +273,14 @@ class SoftmaxLayer(object):
             name='b', borrow=True)
         self.params = [self.w, self.b]
 
-    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+    def set_inpt(self, inpt, mini_batch_size):
         self.inpt = inpt.reshape((mini_batch_size, self.n_in))
-        self.output = softmax((1-self.p_dropout)*T.dot(self.inpt, self.w) + self.b)
+        self.output = softmax(T.dot(self.inpt, self.w) + self.b)
         self.y_out = T.argmax(self.output, axis=1)
-        self.inpt_dropout = dropout_layer(
-            inpt_dropout.reshape((mini_batch_size, self.n_in)), self.p_dropout)
-        self.output_dropout = softmax(T.dot(self.inpt_dropout, self.w) + self.b)
 
     def cost(self, net):
         "Return the log-likelihood cost."
-        return -T.mean(T.log(self.output_dropout)[T.arange(net.y.shape[0]), net.y])
+        return -T.mean(T.log(self.output)[T.arange(net.y.shape[0]), net.y])
 
     def accuracy(self, y):
         "Return the accuracy for the mini-batch."
@@ -304,8 +292,3 @@ def size(data):
     "Return the size of the dataset `data`."
     return data[0].get_value(borrow=True).shape[0]
 
-def dropout_layer(layer, p_dropout):
-    srng = shared_randomstreams.RandomStreams(
-        np.random.RandomState(0).randint(999999))
-    mask = srng.binomial(n=1, p=1-p_dropout, size=layer.shape)
-    return layer*T.cast(mask, theano.config.floatX)
